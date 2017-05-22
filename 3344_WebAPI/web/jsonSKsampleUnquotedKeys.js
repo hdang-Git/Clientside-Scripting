be4f12cf6c733e7deabc3046e1a373b7d7c8127f
hdang-Git <hdang0010@gmail.com>
2017-05-22 16:11:11 -0400

    Lab 6

---
+++ 3344_WebAPI/web/jsonSKsampleUnquotedKeys.js
@@ -0,0 +1,62 @@
+json =
+        {
+            dbError: "",
+            recList: [
+                {
+                    countryId: "1",
+                    countryName: "Afghanistan",
+                    flagAbbrev: "afgh",
+                    flagURL: "http://jpgraph.net/download/manuals/chunkhtml/images/flag-afgh.png",
+                    errorMsg: ""
+                },
+                {
+                    countryId: "2",
+                    countryName: "Alderney",
+                    flagAbbrev: "alde",
+                    flagURL: "http://jpgraph.net/download/manuals/chunkhtml/images/flag-alde.png",
+                    errorMsg: ""
+                },
+                {
+                    countryId: "3",
+                    countryName: "Antarctica",
+                    flagAbbrev: "anta",
+                    flagURL: "http://jpgraph.net/download/manuals/chunkhtml/images/flag-anta.png",
+                    errorMsg: ""
+                },
+                {
+                    countryId: "4",
+                    countryName: "Arab Republic of Egypt",
+                    flagAbbrev: "egyp",
+                    flagURL: "http://jpgraph.net/download/manuals/chunkhtml/images/flag-egyp.png",
+                    errorMsg: ""
+                },
+                {
+                    countryId: "5",
+                    countryName: "Argentine Republic",
+                    flagAbbrev: "arge",
+                    flagURL: "http://jpgraph.net/download/manuals/chunkhtml/images/flag-arge.png",
+                    errorMsg: ""
+                },
+                {
+                    countryId: "6",
+                    countryName: "Aruba",
+                    flagAbbrev: "arub",
+                    flagURL: "http://jpgraph.net/download/manuals/chunkhtml/images/flag-arub.png",
+                    errorMsg: ""
+                },
+                {
+                    countryId: "7",
+                    countryName: "Autonomous Region of Tibet",
+                    flagAbbrev: "tibe",
+                    flagURL: "http://jpgraph.net/download/manuals/chunkhtml/images/flag-tibe.png",
+                    errorMsg: ""
+                },
+                {
+                    countryId: "8",
+                    countryName: "Azerbaijani Republic",
+                    flagAbbrev: "azer",
+                    flagURL: "http://jpgraph.net/download/manuals/chunkhtml/images/flag-azer.png",
+                    errorMsg: ""
+                }
+            ]
+        };
